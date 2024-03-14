726c213ca0afda10168cb8d188b19168c58bdde7
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-14 15:36:41 -0400

    fix: oups commit things

--- src/main.ts
+++ src/main.ts
@@ -7,50 +7,51 @@ import WMTS, { optionsFromCapabilities } from "ol/source/WMTS";
 import WMTSCapabilities from "ol/format/WMTSCapabilities";
 
 import { OpenAPI, DefaultService } from "./client";
+OpenAPI.BASE = "http://points-air.ecolingui.ca/api/v1"
+// For debugging (avoid CORS issues)
+// OpenAPI.BASE = "http://localhost:8092/api/v1"
 
 const repentignyLonLat = [-73.431657, 45.768380]
 
-const parser = new WMTSCapabilities();
-let response = await fetch("./wmts.xml");
-if (!response.ok) throw "Impossible de télécharger les infos WMTS";
-const captext = await response.text();
-const caps = parser.read(captext);
+window.addEventListener("load", initApp);
 
-const options = optionsFromCapabilities(caps, {
-  layer: "carte_gouv_qc",
-  matrixSet: "EPSG_3857",
-});
-if (options === null)
-  throw "Impossible de construire options de WMTS";
+async function initApp() {
+  const plateaux = await DefaultService.activVillePlateauxVilleGet({ ville: "ville-de-repentigny", geometrie: true });
+  const geoj = new GeoJSON();
+  const geojOpts = {
+    featureProjection: "EPSG:3857",
+  };
+  const plateauxSource = new VectorSource({
+    features: plateaux.map(p => geoj.readFeature(p.feature, geojOpts))
+  });
+  const plateauxLayer = new VectorLayer({
+    source: plateauxSource
+  });
 
-options.attributions = "© Gouvernement du Québec";
-options.attributionsCollapsible = false;
-
-/* Pour debug */
-OpenAPI.BASE = "http://localhost:8092/api/v1"
-const plateaux = await DefaultService.activVillePlateauxVilleGet({ville: "ville-de-repentigny", geometrie: true});
-const geoj = new GeoJSON();
-const geojOpts = {
-  featureProjection: "EPSG:3857",
-};
-const plateauxSource = new VectorSource({
-  features: plateaux.map(p => geoj.readFeature(p.feature, geojOpts))
-});
-const plateauxLayer = new VectorLayer({
-  source: plateauxSource
-});
-
-
-new Map({
-  target: "map",
-  layers: [
-    new TileLayer({
-      source: new WMTS(options),
+  const parser = new WMTSCapabilities();
+  let response = await fetch("./wmts.xml");
+  if (!response.ok) throw "Impossible de télécharger les infos WMTS";
+  const captext = await response.text();
+  const caps = parser.read(captext);
+  const options = optionsFromCapabilities(caps, {
+    layer: "carte_gouv_qc",
+    matrixSet: "EPSG_3857",
+  });
+  if (options === null)
+    throw "Impossible de construire options de WMTS";
+  options.attributions = "© Gouvernement du Québec";
+  options.attributionsCollapsible = false;
+  new Map({
+    target: "map",
+    layers: [
+      new TileLayer({
+        source: new WMTS(options),
+      }),
+      plateauxLayer
+    ],
+    view: new View({
+      center: fromLonLat(repentignyLonLat),
+      zoom: 14,
     }),
-    plateauxLayer
-  ],
-  view: new View({
-    center: fromLonLat(repentignyLonLat),
-    zoom: 14,
-  }),
-});
+  });
+}
